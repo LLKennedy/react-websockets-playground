31097f6abac5c7689fe3cf43ec41c12f17f46f84
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-02-21 21:24:17 +1100

    Display message results on-screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export class App extends React.Component<{}, { lastMessage: string, conn: WebSocket }> {
+  constructor(props: {}) {
+    super(props);
 
-let conn = new WebSocket("ws://localhost:9943");
-conn.addEventListener("open", (ev: Event) => {
-  console.log("Opened Websocket: " + ev);
-});
-conn.addEventListener("message", ev => {
-  console.log("Received message: " + ev.data);
-})
-conn.addEventListener("error", ev => {
-  console.log("Received error: " + ev);
-})
-conn.addEventListener("close", ev => {
-  console.log("Received close: " + ev.code);
-})
+    let conn = new WebSocket("ws://localhost:9943");
 
-function App() {
-  console.log()
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      lastMessage: "No messages received",
+      conn: conn
+    };
+
+    conn.addEventListener("open", (ev: Event) => {
+      console.log("Opened Websocket: " + ev);
+    });
+    conn.addEventListener("error", ev => {
+      console.log("Received error: " + ev);
+    })
+    conn.addEventListener("close", ev => {
+      console.log("Received close: " + ev.code);
+    })
+  }
+  componentDidMount() {
+    this.state.conn.addEventListener("message", ev => {
+      this.setState({
+        lastMessage: ev.data,
+      })
+      // console.log("Received message: " + ev.data);
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            {this.state.lastMessage}
+          </p>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
